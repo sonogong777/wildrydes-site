9d9542bdb6edca1def16007642241ef26132ee26
sonogong777 <kenrhee@gmail.com>
2019-07-18 17:53:44 -0400

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_4iQ4hVlQb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '668b9vvjg8gc91bmuab64s5s81', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
